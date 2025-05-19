342c50c26b67c6adcd087ebefcead932d2563a4a
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-05-19 16:17:06 +0300

    Added

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import Nav from './sections/Nav'
+import Hero from './sections/Hero'
+import About from './sections/About'
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <Hero/>
+      <About/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+      <section className='min-h-screen'/>
+    </div>
+  )
+}
+
+export default App
